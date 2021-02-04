73af82d1d9f3617096df4f552c40558f184775b1
LiaoMingRong <849376723@qq.com>
2021-02-04 18:50:23 +0800

    设置robots

--- upload/index.php
+++ upload/index.php
@@ -4,6 +4,5 @@ define('APP_MEMORY_START', memory_get_usage());
 define('APP_TIME_START', microtime(true));
 define('DS', '/');
 define('ROOT_PATH', strtr(realpath(dirname(__FILE__).'/../').'/', '\\', '/'));
-define('APP_TEMPLATE_TYPE', 'storage');
 define('APP_DOMAIN', 'http://upload.example.com/');
 require ROOT_PATH.'frame/start.php';
